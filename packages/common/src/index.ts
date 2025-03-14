1ec0f09a04df69029cb7f523eb6bc6eb4744f3b8
Exelo <exelo.corp@gmail.com>
2025-03-14 17:03:04 +0100

    feat(asset-manager): add asset manager

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./context";
 export * from "./library";
+export * from "./options";
--- packages/common/src/library/libraries/abstracts/graphics.library.abstract.ts
+++ packages/common/src/library/libraries/abstracts/graphics.library.abstract.ts
@@ -1,4 +1,7 @@
+import type { ExecutionContext } from "../../../context";
 import { type IGraphicsLibrary } from "../interfaces";
 import { Library } from "../library";
 
-export abstract class BaseGraphicsLibrary extends Library implements IGraphicsLibrary {}
+export abstract class BaseGraphicsLibrary extends Library implements IGraphicsLibrary {
+  abstract run(context: ExecutionContext): Promise<void>;
+}
