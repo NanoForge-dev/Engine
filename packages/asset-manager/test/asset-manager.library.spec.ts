652dca128b41675c8dc5b36edbcecf50176a2e3d
Exelo <exelo.corp@gmail.com>
2025-03-17 15:14:30 +0100

    feat(asset-manager): add wgsl handling

--- packages/asset-manager/test/asset-manager.library.spec.ts
+++ packages/asset-manager/test/asset-manager.library.spec.ts
@@ -11,28 +11,39 @@ describe("Asset Manager Library", () => {
     canvas: null,
     files: {
       assets: new Map([["/test.png", "blob:http://localhost:3000/test.png"]]),
-      scripts: new Map([["/test.wasm", "blob:http://localhost:3000/test.wasm"]]),
+      wasm: new Map([["/test.wasm", "blob:http://localhost:3000/test.wasm"]]),
+      wgsl: new Map([["/test.wgsl", "blob:http://localhost:3000/test.wgsl"]]),
     },
   });
   library.init(context);
 
-  it("Test get asset", async () => {
+  it("Should get asset", async () => {
     await expect(library.getAsset("test.png")).resolves.toEqual(
       "blob:http://localhost:3000/test.png",
     );
   });
 
-  it("Test get unknown asset", async () => {
+  it("Should throw on unknown asset", async () => {
     await expect(library.getAsset("test-unknown.png")).rejects.toThrow();
   });
 
-  it("Test get script", async () => {
-    await expect(library.getScript("test.wasm")).resolves.toEqual(
+  it("Should get wasm", async () => {
+    await expect(library.getWasm("test.wasm")).resolves.toEqual(
       "blob:http://localhost:3000/test.wasm",
     );
   });
 
-  it("Test get unknown script", async () => {
-    await expect(library.getScript("test-unknown.wasm")).rejects.toThrow();
+  it("Should throw on unknown wasm", async () => {
+    await expect(library.getWasm("test-unknown.wasm")).rejects.toThrow();
+  });
+
+  it("Should get wgsl", async () => {
+    await expect(library.getWgsl("test.wgsl")).resolves.toEqual(
+      "blob:http://localhost:3000/test.wgsl",
+    );
+  });
+
+  it("Should throw on unknown wgsl", async () => {
+    await expect(library.getWgsl("test-unknown.wgsl")).rejects.toThrow();
   });
 });
--- packages/common/src/library/libraries/interfaces/finals/asset-manager.library.type.ts
+++ packages/common/src/library/libraries/interfaces/finals/asset-manager.library.type.ts
@@ -3,5 +3,7 @@ import { type IExposedLibrary } from "../bases/exposed.library.type";
 export interface IAssetManagerLibrary extends IExposedLibrary {
   getAsset(path: string): Promise<string>;
 
-  getScript(path: string): Promise<string>;
+  getWasm(path: string): Promise<string>;
+
+  getWgsl(path: string): Promise<string>;
 }
