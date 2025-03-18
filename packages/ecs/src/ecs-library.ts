8151a7e2e4e63f2a7736ac82e627c9da9882ac38
MartinFillon <martin.fillon@hotmail.com>
2025-03-18 17:20:04 +0100

    feat(ecs): setup wasm to be loaded from path specified in ecs library

--- packages/ecs/src/ecs-library.ts
+++ packages/ecs/src/ecs-library.ts
@@ -1,3 +1,48 @@
-import { type ILibrary } from "@nanoforge/common";
+import type { ILibrary } from "@nanoforge/common";
 
-export class ECSLibrary implements ILibrary {}
+import Module from "../public/libecs.js";
+import type { Entity, MainModule, Registry, SparseArray } from "../public/libecs.js";
+
+function cleanPath(p: string): string {
+  if (p.endsWith("/")) {
+    return p.slice(0, p.length - 1);
+  }
+  return p;
+}
+
+export class ECSLibrary implements ILibrary {
+  name: string = "ECS";
+  module: MainModule;
+  registry: Registry;
+  path: string;
+
+  constructor(path: string) {
+    this.path = path;
+  }
+
+  init(): Promise<void> {
+    return Module({ locateFile: (file: string) => `${cleanPath(this.path)}/${file}` }).then(
+      (module) => {
+        this.module = module;
+        this.registry = new module.Registry();
+        return Promise.resolve();
+      },
+    );
+  }
+
+  clear(): Promise<void> {
+    return Promise.resolve();
+  }
+
+  addComponent(entity: Entity, component: any): void {
+    this.registry.add_component(entity, component);
+  }
+
+  createEntity(): Entity {
+    return this.registry.spawn_entity();
+  }
+
+  getComponents(component: any): SparseArray {
+    return this.registry.get_components(component);
+  }
+}
