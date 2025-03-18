5df042fe58fbe6e3de906db409919d9519c273d9
MartinFillon <martin.fillon@hotmail.com>
2025-03-18 17:20:04 +0100

    feat(ecs): setup wasm to be loaded from path specified in ecs library

---
+++ packages/ecs/test/ecs-library.spec.ts
@@ -0,0 +1,38 @@
+import { ECSLibrary } from "../src/ecs-library";
+
+class Position {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+describe("ECSLibrary", () => {
+  test("init and spawn entity", async () => {
+    const ecs = new ECSLibrary("public");
+    await ecs.init();
+    const entity = ecs.createEntity();
+    expect(entity).toBeDefined();
+    expect(entity.get_id()).toBe(0);
+  });
+
+  test("add component to entity", async () => {
+    const ecs = new ECSLibrary("public/");
+    await ecs.init();
+    const entity = ecs.createEntity();
+    const pos = new Position(1, 2);
+    ecs.addComponent(entity, pos);
+    const components = ecs.getComponents(Position);
+    expect(components.get(entity.get_id())).toStrictEqual(new Position(1, 2));
+    expect(components.size()).toBe(1);
+  });
+
+  test("clear", async () => {
+    const ecs = new ECSLibrary("public/");
+    await ecs.init();
+    await ecs.clear();
+  });
+});
