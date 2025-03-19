ed18a64e3a1e85424f0aa54f89b675b08674137e
Exelo <exelo.corp@gmail.com>
2025-03-19 13:36:14 +0100

    feat(game): add template

---
+++ example/template/src/index.ts
@@ -0,0 +1,15 @@
+import { AssetManagerLibrary } from "@nanoforge/asset-manager";
+import { type IRunOptions } from "@nanoforge/common";
+import { NanoforgeFactory } from "@nanoforge/core";
+import { ECSLibrary } from "@nanoforge/ecs";
+import { Graphics2DLibrary } from "@nanoforge/graphics-2d";
+
+export const app = NanoforgeFactory.createClient();
+
+export const main = async (options: IRunOptions) => {
+  app.useGraphics(new Graphics2DLibrary());
+  app.useAssetManager(new AssetManagerLibrary());
+  app.useComponentSystem(new ECSLibrary());
+
+  app.run(options);
+};
