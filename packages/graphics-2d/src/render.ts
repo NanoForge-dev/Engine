0a1e1b70a85a494d58a744348f8aed18ff56a699
Exelo <exelo.corp@gmail.com>
2025-03-19 14:18:04 +0100

    feat(game): add graphics factory

--- packages/graphics-2d/src/render.ts
+++ packages/graphics-2d/src/render.ts
@@ -1,17 +1,15 @@
 import { type InitContext } from "@nanoforge/common";
 
+import { type NfgComponent } from "./components/component";
 import { type GraphicsCore } from "./core";
 
 export class GraphicsRender {
   private readonly _core: GraphicsCore;
-  private readonly _canvas: HTMLCanvasElement;
   private readonly _canvasContext: GPUCanvasContext;
   private readonly _canvasFormat: GPUTextureFormat;
 
   constructor(core: GraphicsCore, initContext: InitContext) {
     this._core = core;
-
-    this._canvas = initContext.canvas;
 
     const context = initContext.canvas.getContext("webgpu");
     if (!context) {
@@ -27,11 +25,40 @@ export class GraphicsRender {
     });
   }
 
-  get canvasContext(): GPUCanvasContext {
-    return this._canvasContext;
-  }
-
   get canvasFormat(): GPUTextureFormat {
     return this._canvasFormat;
+  }
+
+  render(components: NfgComponent[]): void {
+    const [encoder, pass] = this._beginRender();
+    this._renderComponents(pass, components);
+    this._endRender(encoder);
+  }
+
+  private _beginRender(): [GPUCommandEncoder, GPURenderPassEncoder] {
+    const encoder = this._core.device.createCommandEncoder();
+
+    const pass = encoder.beginRenderPass({
+      colorAttachments: [
+        {
+          view: this._canvasContext.getCurrentTexture().createView(),
+          loadOp: "clear",
+          clearValue: { r: 0, g: 0, b: 0.4, a: 1.0 },
+          storeOp: "store",
+        },
+      ],
+    });
+
+    return [encoder, pass];
+  }
+
+  private _renderComponents(pass: GPURenderPassEncoder, components: NfgComponent[]): void {
+    for (const component of components) {
+      component.draw(pass);
+    }
+  }
+
+  private _endRender(encoder: GPUCommandEncoder): void {
+    this._core.device.queue.submit([encoder.finish()]);
   }
 }
