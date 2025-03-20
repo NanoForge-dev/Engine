d480b48263acfb8fccc95888b76b824bea6428e5
bill <timothe.jacquot@epitech.eu>
2025-03-20 18:03:25 +0100

    feat(game): create pong players with background

--- example/pong/src/index.ts
+++ example/pong/src/index.ts
@@ -4,8 +4,8 @@ import { NanoforgeFactory } from "@nanoforge/core";
 import { ECSLibrary } from "@nanoforge/ecs";
 import { Graphics2DLibrary } from "@nanoforge/graphics-2d";
 
-import { Bounce, CircleComponent, Position, Velocity } from "./components";
-import { bounce, draw, framerate, move } from "./systems";
+import { Bounce, CircleComponent, Position, RectangleComponent, Velocity } from "./components";
+import { bounce, drawCircle, drawRectangle, framerate, move } from "./systems";
 
 export const ecsLibrary = new ECSLibrary();
 
@@ -20,8 +20,7 @@ export const main = async (options: IRunOptions) => {
   await app.init(options);
 
   const ball = ecsLibrary.createEntity();
-
-  ecsLibrary.addComponent(ball, new Velocity(0.05, 0.05));
+  ecsLibrary.addComponent(ball, new Velocity(0.04, 0));
   ecsLibrary.addComponent(ball, new Position(0.5, 0));
   ecsLibrary.addComponent(ball, new Bounce());
   ecsLibrary.addComponent(
@@ -33,8 +32,46 @@ export const main = async (options: IRunOptions) => {
       }),
     ),
   );
+
+  const bg = ecsLibrary.createEntity();
+  ecsLibrary.addComponent(
+    bg,
+    new RectangleComponent(
+      await graphics.factory.createRectangle({
+        min: { x: -2, y: -1 },
+        max: { x: 2, y: 1 },
+        color: { r: 0, g: 0, b: 0, a: 0 },
+      }),
+    ),
+  );
+
+  const player1 = ecsLibrary.createEntity();
+  ecsLibrary.addComponent(
+    player1,
+    new RectangleComponent(
+      await graphics.factory.createRectangle({
+        min: { x: -1.8, y: -0.3 },
+        max: { x: -1.7, y: 0.3 },
+        color: { r: 0, g: 0, b: 1, a: 1 },
+      }),
+    ),
+  );
+
+  const player2 = ecsLibrary.createEntity();
+  ecsLibrary.addComponent(
+    player2,
+    new RectangleComponent(
+      await graphics.factory.createRectangle({
+        min: { x: 1.7, y: -0.3 },
+        max: { x: 1.8, y: 0.3 },
+        color: { r: 0, g: 0, b: 1, a: 1 },
+      }),
+    ),
+  );
+
   ecsLibrary.addSystem(move);
-  ecsLibrary.addSystem(draw);
+  ecsLibrary.addSystem(drawRectangle);
+  ecsLibrary.addSystem(drawCircle);
   ecsLibrary.addSystem(bounce);
   ecsLibrary.addSystem(() => framerate(30));
 
