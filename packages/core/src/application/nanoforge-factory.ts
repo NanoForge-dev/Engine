7d9da69be4301875020176656276236b88b737f1
Exelo <exelo.corp@gmail.com>
2025-03-25 12:11:33 +0100

    feat(core): add default libraries to constructor

--- packages/core/src/application/nanoforge-factory.ts
+++ packages/core/src/application/nanoforge-factory.ts
@@ -1,14 +1,22 @@
+import { AssetManagerLibrary } from "@nanoforge/asset-manager";
+import { InputLibrary } from "@nanoforge/input";
+
 import { type IApplicationOptions } from "./application-options.type";
 import { NanoforgeClient } from "./nanoforge-client";
 import { NanoforgeServer } from "./nanoforge-server";
 
 class NanoforgeFactoryStatic {
   createClient(options?: Partial<IApplicationOptions>): NanoforgeClient {
-    return new NanoforgeClient(options);
+    const app = new NanoforgeClient(options);
+    app.useAssetManager(new AssetManagerLibrary());
+    app.useInput(new InputLibrary());
+    return app;
   }
 
   createServer(options?: Partial<IApplicationOptions>): NanoforgeServer {
-    return new NanoforgeServer(options);
+    const app = new NanoforgeServer(options);
+    app.useAssetManager(new AssetManagerLibrary());
+    return app;
   }
 }
 
