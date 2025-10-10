53c797fcc44c90609503ae3236530c8d18701ded
Exelo <exelo.corp@gmail.com>
2025-10-10 11:58:38 +0200

    test(ecs): update `ecs-library` tests to use `ECSRegistry` for entity and component management

--- packages/ecs/test/ecs-library.spec.ts
+++ packages/ecs/test/ecs-library.spec.ts
@@ -6,6 +6,7 @@ import {
   InitContext,
 } from "@nanoforge/common";
 import { EditableLibraryManager } from "@nanoforge/core/src/common/library/manager/library.manager";
+import { type ECSRegistry } from "@nanoforge/ecs";
 import { ECSLibrary } from "@nanoforge/ecs/src/ecs-library";
 
 class Position {
@@ -21,6 +22,7 @@ class Position {
 
 describe("ECSLibrary", () => {
   let ecs: ECSLibrary;
+  let registry: ECSRegistry;
   const assetManager = new AssetManagerLibrary();
   const appContext = new ApplicationContext();
   const libraryManager = new EditableLibraryManager();
@@ -45,19 +47,20 @@ describe("ECSLibrary", () => {
   beforeEach(async () => {
     ecs = new ECSLibrary();
     await ecs.init(initContext);
+    registry = ecs.registry;
   });
 
   test("init and spawn entity", async () => {
-    const entity = ecs.spawnEntity();
+    const entity = registry.spawnEntity();
     expect(entity).toBeDefined();
     expect(entity.getId()).toBe(0);
   });
 
   test("add component to entity", async () => {
-    const entity = ecs.spawnEntity();
+    const entity = registry.spawnEntity();
     const pos = new Position(1, 2);
-    ecs.addComponent(entity, pos);
-    const components = ecs.getComponents(Position);
+    registry.addComponent(entity, pos);
+    const components = registry.getComponents(Position);
     expect(components.get(entity.getId())).toStrictEqual(new Position(1, 2));
     expect(components.size()).toBe(1);
   });
