38646439ed40dac4d190829e3839228d0e2605fa
Exelo <exelo.corp@gmail.com>
2025-11-10 08:38:04 +0100

    test: refactor context initialization and asset handling in library tests

--- packages/asset-manager/test/asset-manager.library.spec.ts
+++ packages/asset-manager/test/asset-manager.library.spec.ts
@@ -15,41 +15,35 @@ describe("Asset Manager Library", () => {
   const context = new InitContext(appContext, libraryManager, configRegistry, {
     // @ts-ignore
     canvas: null,
-    files: {
-      assets: new Map([["/test.png", "blob:http://localhost:3000/test.png"]]),
-      wasm: new Map([["/test.wasm", "blob:http://localhost:3000/test.wasm"]]),
-      wgsl: new Map([["/test.wgsl", "blob:http://localhost:3000/test.wgsl"]]),
-    },
+    files: new Map([
+      ["/test.png", "blob:http://localhost:3000/test.png"],
+      ["/test.wasm", "blob:http://localhost:3000/test.wasm"],
+      ["/test.wgsl", "blob:http://localhost:3000/test.wgsl"],
+    ]),
   });
   library.__init(context);
 
-  it("Should get asset", async () => {
-    expect((await library.getAsset("test.png")).path).toEqual(
-      "blob:http://localhost:3000/test.png",
-    );
+  it("Should get asset", () => {
+    expect(library.getAsset("test.png").path).toEqual("blob:http://localhost:3000/test.png");
   });
 
-  it("Should throw on unknown asset", async () => {
-    await expect(library.getAsset("test-unknown.png")).rejects.toThrow();
+  it("Should throw on unknown asset", () => {
+    expect(() => library.getAsset("test-unknown.png")).toThrow();
   });
 
-  it("Should get wasm", async () => {
-    expect((await library.getWasm("test.wasm")).path).toEqual(
-      "blob:http://localhost:3000/test.wasm",
-    );
+  it("Should get wasm", () => {
+    expect(library.getAsset("test.wasm").path).toEqual("blob:http://localhost:3000/test.wasm");
   });
 
-  it("Should throw on unknown wasm", async () => {
-    await expect(library.getWasm("test-unknown.wasm")).rejects.toThrow();
+  it("Should throw on unknown wasm", () => {
+    expect(() => library.getAsset("test-unknown.wasm")).toThrow();
   });
 
-  it("Should get wgsl", async () => {
-    expect((await library.getWgsl("test.wgsl")).path).toEqual(
-      "blob:http://localhost:3000/test.wgsl",
-    );
+  it("Should get wgsl", () => {
+    expect(library.getAsset("test.wgsl").path).toEqual("blob:http://localhost:3000/test.wgsl");
   });
 
-  it("Should throw on unknown wgsl", async () => {
-    await expect(library.getWgsl("test-unknown.wgsl")).rejects.toThrow();
+  it("Should throw on unknown wgsl", () => {
+    expect(() => library.getAsset("test-unknown.wgsl")).toThrow();
   });
 });
