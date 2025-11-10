4717c5a216579e8368de5a3de9478a23af2ac0f2
Exelo <exelo.corp@gmail.com>
2025-11-10 07:56:43 +0100

    refactor(asset-manager): simplify the usage of asset manager

--- packages/common/src/library/libraries/interfaces/finals/asset-manager.library.type.ts
+++ packages/common/src/library/libraries/interfaces/finals/asset-manager.library.type.ts
@@ -1,3 +1,6 @@
+import { type NfFile } from "../../../../common";
 import { type IExposedLibrary } from "../bases/exposed.library.type";
 
-export interface IAssetManagerLibrary extends IExposedLibrary {}
+export interface IAssetManagerLibrary extends IExposedLibrary {
+  getAsset(path: string): NfFile;
+}
