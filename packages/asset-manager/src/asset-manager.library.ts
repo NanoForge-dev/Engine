8759e958a0c66e3c6e36fbea1a8ee6a305f86dc0
Exelo <exelo.corp@gmail.com>
2025-11-10 07:56:43 +0100

    refactor(asset-manager): simplify the usage of asset manager

--- packages/asset-manager/src/asset-manager.library.ts
+++ packages/asset-manager/src/asset-manager.library.ts
@@ -1,45 +1,18 @@
-import { BaseAssetManagerLibrary, type InitContext } from "@nanoforge/common";
-
-import { NfFile } from "./file";
+import { BaseAssetManagerLibrary, type InitContext, NfFile } from "@nanoforge/common";
 
 export class AssetManagerLibrary extends BaseAssetManagerLibrary {
   private _assets: Map<string, string>;
-  private _wasm: Map<string, string>;
-  private _wgsl: Map<string, string>;
 
   get __name(): string {
     return "AssetManagerLibrary";
   }
 
   public async __init(context: InitContext): Promise<void> {
-    this._assets = context.files.assets;
-    this._wasm = context.files.wasm;
-    this._wgsl = context.files.wgsl;
+    this._assets = context.files;
   }
 
-  /**
-   * @todo Error management
-   */
-  public async getAsset(path: string): Promise<NfFile> {
+  public getAsset(path: string): NfFile {
     const res = this._assets.get(this._parsePath(path));
-    if (!res) throw new Error("Asset not found.");
-    return new NfFile(res);
-  }
-
-  /**
-   * @todo Error management
-   */
-  public async getWasm(path: string): Promise<NfFile> {
-    const res = this._wasm.get(this._parsePath(path));
-    if (!res) throw new Error("Asset not found.");
-    return new NfFile(res);
-  }
-
-  /**
-   * @todo Error management
-   */
-  public async getWgsl(path: string): Promise<NfFile> {
-    const res = this._wgsl.get(this._parsePath(path));
     if (!res) throw new Error("Asset not found.");
     return new NfFile(res);
   }
