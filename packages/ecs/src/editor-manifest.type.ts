e26319c4b616bc36d7610743a3a873d8be5d2097
Exelo <exelo.corp@gmail.com>
2025-11-29 05:49:27 +0100

    feat(ecs): add types for NanoForge Editor

---
+++ packages/ecs/src/editor-manifest.type.ts
@@ -0,0 +1,94 @@
+/**
+ * * Editor Component Element Defaults
+ * Basic fields for Editor Component Element
+ * @param Type - Type of the element
+ * @param Default - Type of the element's default value
+ */
+type ECElementDefaults<Type, Default> = {
+  /**
+   * Type of the element
+   */
+  type: Type;
+
+  /**
+   * Is the element optional
+   * @default false
+   */
+  optional?: boolean;
+
+  /**
+   * Default value of the element
+   * Force optional to true if set
+   */
+  default?: Default;
+};
+
+/**
+ * * Editor Component String Element
+ * Type for string element
+ */
+type ECStringElement = {
+  /**
+   * Values allowed for the element
+   */
+  enum?: string[];
+} & ECElementDefaults<"string", string>;
+
+/**
+ * * Editor Component Number Element
+ * Type for number element
+ */
+type ECNumberElement = ECElementDefaults<"number", number>;
+
+/**
+ * * Editor Component Boolean Element
+ * Type for boolean element
+ */
+type ECBooleanElement = ECElementDefaults<"boolean", boolean>;
+
+/**
+ * * Editor Component Array Element
+ * Type for array element
+ */
+type ECArrayElement = {
+  /**
+   * Items of the array
+   */
+  items: ECElement;
+} & ECElementDefaults<"array", any[]>;
+
+/**
+ * * Editor Component Object Element
+ * Type for object element
+ */
+type ECObjectElement = {
+  /**
+   * Properties of the object
+   */
+  properties: Record<string, ECElement>;
+} & ECElementDefaults<"object", object>;
+
+/**
+ * * Editor Component Element
+ * Type for component element
+ */
+type ECElement =
+  | ECStringElement
+  | ECNumberElement
+  | ECBooleanElement
+  | ECArrayElement
+  | ECObjectElement;
+
+/**
+ * Manifest for a component to be used in the NanoForge Editor
+ */
+export type EditorComponentManifest = {
+  params: ECElement[];
+};
+
+/**
+ * Manifest for a system to be used in the NanoForge Editor
+ */
+export type EditorSystemManifest = {
+  dependencies: string[];
+};
--- packages/ecs/src/index.ts
+++ packages/ecs/src/index.ts
@@ -2,3 +2,4 @@ import "../lib/libecs.wasm";
 
 export { ECSLibrary } from "./ecs-library";
 export type { Component, System, Registry } from "../lib";
+export type * from "./editor-manifest.type";
