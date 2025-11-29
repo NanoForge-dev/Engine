cfd5e79e1563b4a13af9d3397051f69b43ef708c
Exelo <exelo.corp@gmail.com>
2025-11-29 04:45:10 +0100

    chore(config): update release configuration and remove unused dependencies

--- packages/config/tsup.config.ts
+++ packages/config/tsup.config.ts
@@ -1,9 +1,3 @@
-import { esbuildPluginVersionInjector } from "esbuild-plugin-version-injector";
-
 import { createTsupConfig } from "../../tsup.config.js";
 
-export default [
-  createTsupConfig({
-    esbuildPlugins: [esbuildPluginVersionInjector()],
-  }),
-];
+export default [createTsupConfig()];
