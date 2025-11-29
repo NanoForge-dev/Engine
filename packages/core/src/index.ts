b9923064ba1da3164b1739fcdec5a819734c4ba2
Exelo <exelo.corp@gmail.com>
2025-11-29 10:41:02 +0100

    feat: add schematics used types (#102)
    * feat(ecs): add types for NanoForge Editor
    
    * feat(core): add types for init functions in schematics
    
    * docs(ecs): add `params` and `dependencies` fields doc
    
    * feat(ecs): extend ECElement type with `name`, `description`, `example`, and updated `params` def
    
    * feat(ecs): add `name` and `description` fields to Editor manifests

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,4 @@
 export * from "./application/nanoforge-factory";
+
+export type { NanoforgeClient } from "./application/nanoforge-client";
+export type { NanoforgeServer } from "./application/nanoforge-server";
