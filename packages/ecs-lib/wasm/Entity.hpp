15ba38fb7e0ac0e245ddd09e7e590e472d956861
MartinFillon <martin.fillon@hotmail.com>
2025-11-30 11:20:35 +0100

    docs(ecs): document ecs and design choices

--- packages/ecs-lib/wasm/Entity.hpp
+++ packages/ecs-lib/wasm/Entity.hpp
@@ -18,13 +18,29 @@
 namespace nfo {
     class Entity {
       public:
+        /**
+         * Create an entity from an ID.
+         *
+         * @param id The ID of the entity.
+         * @return An Entity instance.
+         */
         explicit Entity(const std::size_t id) : _id(id) {}
 
+        /**
+         * Convert the entity to its ID.
+         *
+         * @return The ID of the entity.
+         */
         operator std::size_t() const
         {
             return _id;
         }
 
+        /**
+         * Get the ID of the entity.
+         *
+         * @return The ID of the entity.
+         */
         [[nodiscard]] std::size_t get_id() const
         {
             return _id;
