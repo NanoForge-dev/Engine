23313f6321c3f47d1fb52dc68a21d3ea3b15fe86
Exelo <exelo.corp@gmail.com>
2025-12-02 04:08:38 +0100

    refactor(core): remove default libs in factory

--- packages/core/src/application/nanoforge-factory.ts
+++ packages/core/src/application/nanoforge-factory.ts
@@ -1,22 +1,14 @@
-import { AssetManagerLibrary } from "@nanoforge-dev/asset-manager";
-import { InputLibrary } from "@nanoforge-dev/input";
-
 import { type IApplicationOptions } from "./application-options.type";
 import { NanoforgeClient } from "./nanoforge-client";
 import { NanoforgeServer } from "./nanoforge-server";
 
 class NanoforgeFactoryStatic {
   createClient(options?: Partial<IApplicationOptions>): NanoforgeClient {
-    const app = new NanoforgeClient(options);
-    app.useAssetManager(new AssetManagerLibrary());
-    app.useInput(new InputLibrary());
-    return app;
+    return new NanoforgeClient(options);
   }
 
   createServer(options?: Partial<IApplicationOptions>): NanoforgeServer {
-    const app = new NanoforgeServer(options);
-    app.useAssetManager(new AssetManagerLibrary());
-    return app;
+    return new NanoforgeServer(options);
   }
 }
 
