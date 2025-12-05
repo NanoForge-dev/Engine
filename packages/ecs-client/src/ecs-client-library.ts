f8dcc5472dc5b7b86ad6af3482d1ff2646a70aac
Exelo <exelo.corp@gmail.com>
2025-12-05 11:01:54 +0100

    feat: add ecs client library

---
+++ packages/ecs-client/src/ecs-client-library.ts
@@ -0,0 +1,21 @@
+import { type InitContext } from "@nanoforge-dev/common";
+import { AbstractECSLibrary } from "@nanoforge-dev/ecs";
+
+import { default as Module } from "../lib/libecs";
+
+export class ECSClientLibrary extends AbstractECSLibrary {
+  constructor() {
+    super();
+    this.path = "libecs.wasm";
+  }
+
+  get __name(): string {
+    return "ECSLibrary";
+  }
+
+  override async __init(context: InitContext): Promise<void> {
+    const wasmFile = context.libraries.getAssetManager().library.getAsset(this.path);
+    this.module = await Module({ locateFile: () => wasmFile.path });
+    this._registry = new this.module.Registry();
+  }
+}
---
+++ packages/ecs-client/src/index.ts
@@ -0,0 +1,4 @@
+import "../lib/libecs.wasm";
+
+export { ECSClientLibrary } from "./ecs-client-library";
+export type { Component, System, Registry } from "../lib/libecs";
