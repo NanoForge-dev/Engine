c7c5d5f20cf0b2ba9090206a80a0887abe6ef13d
Exelo <exelo.corp@gmail.com>
2025-12-07 06:09:00 +0100

    chore: change eslint and prettier configs imports

--- utils/eslint-config/index.js
+++ utils/eslint-config/index.js
@@ -1,6 +1,5 @@
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import pluginJest from "eslint-plugin-jest";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -41,20 +40,6 @@ const eslintConfig = [
           ],
         },
       ],
-    },
-  },
-  {
-    files: ["**/*.spec.ts"],
-    plugins: { jest: pluginJest },
-    languageOptions: {
-      globals: pluginJest.environments.globals.globals,
-    },
-    rules: {
-      "jest/no-disabled-tests": "warn",
-      "jest/no-focused-tests": "error",
-      "jest/no-identical-title": "error",
-      "jest/prefer-to-have-length": "warn",
-      "jest/valid-expect": "error",
     },
   },
 ];
