1799e1a431b4d9b9d577e0678d9d382a1dc2ef11
Tchips46 <leo.out46@gmail.com>
2026-04-14 16:20:39 +0900

    fix(lib-ecs): review

--- packages/ecs-lib/src/index.ts
+++ packages/ecs-lib/src/index.ts
@@ -1,4 +1,3 @@
 export { AbstractECSLibrary } from "./ecs-library.abstract";
-// @ts-ignore
 export type { Component, System, Registry, SparseArray, Entity } from "../lib/libecs";
 export type * from "./editor-manifest.type";
