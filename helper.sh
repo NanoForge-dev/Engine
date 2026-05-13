c89a039022f142d833db9c25595f09592165d461
MartinFillon <martin.fillon@hotmail.com>
2026-05-13 11:50:52 +0200

    chore: remove typedoc

--- helper.sh
+++
@@ -1,6 +0,0 @@
-for package in packages/*; do
-  packageName=$(basename $package)
-  echo "Building $package"
-  pnpm exec mint-tsdocs init -p $package --yes
-  jq '.outputFolder="../../docs/'$packageName'"' "$package/mint-tsdocs.config.json" > tmp.json && mv tmp.json $package/mint-tsdocs.config.json
-done
