ad2b8e89724c137b1a8a360ab20e4201e2fbbc21
Exelo <exelo.corp@gmail.com>
2026-09-12 16:26:21 +0200

    feat: add ecs editor manifest types

--- modules/ecs/tsdown.config.ts
+++ modules/ecs/tsdown.config.ts
@@ -1,6 +1,7 @@
 import { createTsdownConfig } from "../../tsdown.config";
 
 export default [
+  createTsdownConfig(),
   createTsdownConfig({ entry: "src/client/index.ts", outDir: "dist/client" }),
   createTsdownConfig({ entry: "src/server/index.ts", outDir: "dist/server" }),
 ];
