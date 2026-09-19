2ccccc26fcb12d1698c34023fabcc8c713e7104a
Exelo <exelo.corp@gmail.com>
2026-09-19 23:45:57 +0200

    fix: change tsdown config to add wasm import in js dist file

--- modules/ecs/tsdown.config.ts
+++ modules/ecs/tsdown.config.ts
@@ -1,7 +1,15 @@
+import { type UserConfig } from "tsdown";
+
 import { createTsdownConfig } from "../../tsdown.config";
 
-export default [
-  createTsdownConfig(),
-  createTsdownConfig({ entry: "src/client/index.ts", outDir: "dist/client" }),
-  createTsdownConfig({ entry: "src/server/index.ts", outDir: "dist/server" }),
-];
+const createECSPartConfig = (part: "client" | "server"): UserConfig =>
+  createTsdownConfig({
+    entry: `src/${part}/index.ts`,
+    outDir: `dist/${part}`,
+    copy: [`lib/${part === "client" ? "web" : "node"}/libecs.wasm`],
+    banner: ({ format }) => ({
+      js: format === "cjs" ? 'require("./libecs.wasm");' : 'import "./libecs.wasm";',
+    }),
+  });
+
+export default [createTsdownConfig(), createECSPartConfig("client"), createECSPartConfig("server")];
